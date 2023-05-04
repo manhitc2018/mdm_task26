af4298ee78d31ae580585840e74c3784108e8263
manhitc2018 <manhitc2018@gmail.com>
2023-05-04 07:00:35 +0700

    Home

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("main.js");
+$(document).ready(function(){
+    $('.c-design__sliderContent').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        prevArrow: $(".c-control__itemPre"),
+        nextArrow: $(".c-control__itemNext"),
+    });
+  });
